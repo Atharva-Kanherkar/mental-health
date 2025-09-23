e61274d637d5067c98f3046fb84062ee78146c23
Atharva-Kanerkar <atharvakanherkar25@gmail.com>
2025-09-23 13:42:45 +0530

    Add cors

--- src/server.ts
+++ src/server.ts
@@ -19,7 +19,11 @@ import userRoutes from "./routes/user";
 const app = express();
  
 app.use(cors({
-    origin: process.env.FRONTEND_URL  || "http://localhost:3000",
+    origin: [
+    "http://localhost:3000",
+    "https://www.my-echoes.app",
+    process.env.FRONTEND_URL || ""
+  ],
     credentials: true,
 }));
 
